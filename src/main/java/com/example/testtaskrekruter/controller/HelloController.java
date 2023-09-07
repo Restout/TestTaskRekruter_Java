6480a74e1eecd2a0ff66c05b622bff4f2f820f88
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-09-07 12:59:50 +0300

    Create: hello controller to print hello message

--- src/main/java/com/example/testtaskrekruter/controller/HelloController.java
+++ src/main/java/com/example/testtaskrekruter/controller/HelloController.java
@@ -1,4 +1,24 @@
 package com.example.testtaskrekruter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HelloController {
+    @GetMapping("/")
+    public ResponseEntity<String> getHelloMessage(@RequestParam String name, @RequestParam String message) {
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder
+                .append("Hello ")
+                .append(name)
+                .append("!")
+                .append(message)
+                .append("!");
+
+        return ResponseEntity
+                .ok()
+                .body(messageBuilder.toString());
+    }
 }
